d1b93b2348c656396ab8bc3f95fe6c07aecdd9e7
wadenya <alexwadenya001@gmail.com>
2023-03-07 06:44:56 -0800

    check

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for letter in range(97, 123):
     if chr(letter) == 'q' or chr(letter) == 'e':
         continue
-    print(chr(letter).format(letter),end='')
+    print(chr(letter).format(letter), end='')
