86912d7953223751b0e2fb9f6c7bd86d051297e5
wadenya <alexwadenya001@gmail.com>
2023-03-07 01:18:37 -0800

    corrected

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = The Zen of Python, by Tim Peters
+name = """The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -19,5 +19,5 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-print('str')
+Namespaces are one honking great idea -- let's do more of those!"""
+print(name)
