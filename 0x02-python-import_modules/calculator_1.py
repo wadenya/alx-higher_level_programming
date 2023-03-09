ff1189710591a75a3da63bb293f5877c387242e3
wadenya <alexwadenya001@gmail.com>
2023-03-09 06:53:41 -0800

    clear

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,49 +1,13 @@
 #!/usr/bin/python3
 def add(a, b):
-    """My addition function
+    return(a + b)
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
+def sub(a, b):        
     return (a - b)
 
 def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
     return (a * b)
+
 def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
     return int(a / b)
 
