8f473a6c61f4894be0ce46866ceb5740cdd1922b
wadenya <alexwadenya001@gmail.com>
2023-03-11 01:48:46 -0800

    one 2 seven

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers."""
     for y in matrix:
-        print(" ".join("{:d}".format(s) for sin y)
+        print(" ".join("{:d}".format(s) for s in y)
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,11 +8,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif lenA < 2:
         tuple_a = (tuple_a[0], 0)
-#chaeck for tuple_b
+#check for tuple_b
         if lenB < 1:
             tuple_b = (0, 0)
-        elif lenB <2:
+        elif lenB < 2:
             tuple_b = (tuple_b[0], 0)
 
-        res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return res
+        rs = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        return rs
