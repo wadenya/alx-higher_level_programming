64af3e700288e741665981c43f4d1f0237687862
wadenya <alexwadenya001@gmail.com>
2023-03-15 05:34:39 -0700

    second trial

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,13 +1,14 @@
 #include "/usr/include/python3.4/Python.h"
 #include <stdio.h>
 
-void print_hexn(const char *str, int h)
+void print_hexn(const char *str, int n)
 {
-	int y = 0;
+	int i = 0;
 
-	for (; y < h - 1; ++y)
-		printf("%02x ", (unsigned char) str[y]);
-	printf("%02x", str[y]);
+	for (; i < n - 1; ++i)
+		printf("%02x ", (unsigned char) str[i]);
+
+	printf("%02x", str[i]);
 }
 
 void print_python_bytes(PyObject *p)
@@ -20,6 +21,9 @@ void print_python_bytes(PyObject *p)
 	{
 		clone_size = PyBytes_Size(p);
 		calc_bytes = clone_size + 1;
+
+		if (calc_bytes >= 10)
+			calc_bytes = 10;
 
 		printf("  size: %d\n", clone_size);
 		printf("  trying string: %s\n", clone->ob_sval);
