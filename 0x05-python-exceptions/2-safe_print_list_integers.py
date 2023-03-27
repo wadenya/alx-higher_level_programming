5a58f655347c4db6f9d115501ecc549c55c5f8bf
wadenya <alexwadenya001@gmail.com>
2023-03-27 04:36:35 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     nt = 0
     for i in range(x):
         try:
-            print("{:d}".format([i]), end="\n")
+            print("{:d}".format([i]), end="")
             nt += 1
         except(ValueError, TypeError):
             continue
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             result = 0
         except IndexError:
-            print("Out of range")
+            print("out of range")
             result = 0
         finally:
             new_list.append(result)
