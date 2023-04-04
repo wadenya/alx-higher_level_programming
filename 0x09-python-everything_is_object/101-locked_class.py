0fba7ac3c1b48545044e62fa631fc94bee31f252
wadenya <alexwadenya001@gmail.com>
2023-04-04 13:04:18 -0700

    More and more objects

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slot__ = ["first_name"]
+"""Defines locked class"""
 
-    def __int__(self):
-        self.first_name = None
+class LockedClass:
+    """no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name"""
+    __slot__ = ["first_name
