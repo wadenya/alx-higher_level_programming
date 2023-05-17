e20fb7f32736a31d73592a596aa019d90519db50
wadenya <alexwadenya001@gmail.com>
2023-05-17 08:31:52 -0700

    SQL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL
-SELECT s.'title', g.'genre_id'
-FROM 'tv_shows' AS s
-	INNER JOIN 'tv_show_genres' AS g
-	ON s.'id' = g.'show_id'
-ORDER BY s. 'title', g.'genre_id';
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+	INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ORDER BY s.`title`, g.`genre_id`;
