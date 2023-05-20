dd317db4d3da4002c33119414a77a5db5db59aac
wadenya <alexwadenya001@gmail.com>
2023-05-19 23:30:12 -0700

    remove

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++
@@ -1,6 +0,0 @@
--- Import the database dump of hbtn_0d_tvshows to your MySQL
-SELECT s.`title`, g.`genre_id`
-FROM `tv_shows` AS s
-	LEFT JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
-ORDER BY s.`title`, g.`genre_id`;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x00. Python - Hello, World
+0x0E-SQL_more_queries
