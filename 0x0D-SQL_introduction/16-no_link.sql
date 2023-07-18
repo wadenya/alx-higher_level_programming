6b67e46abe4d96d778ff370ebc0d257b2ad7cdf6
wadenya <alexwadenya001@gmail.com>
2023-07-18 07:55:05 -0700

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table
+-- Don’t list rows without a name value
+SELECT 'score', 'name'
+FROM 'second-table'
+WHERE 'name' != ""
+ORDER BY 'score' DESC
