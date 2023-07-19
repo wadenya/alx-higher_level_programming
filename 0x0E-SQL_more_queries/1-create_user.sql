dbfbacb1e005d200c72b88a71cc3879608d744cb
wadenya <alexwadenya001@gmail.com>
2023-07-19 06:58:44 -0700

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script that creates the MySQL server user user_0d_1
+-- The user_0d_1 password should be set to user_0d_1_pwd
+CREATE USER IP NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost
