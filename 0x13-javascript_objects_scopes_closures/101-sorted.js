45d8be284e92b3fcee0bf52d82a7530b89aa754a
wadenya <alexwadenya001@gmail.com>
2023-08-16 04:03:51 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,16 +3,16 @@
 const dict = require('./101-data').dict;
 
 const totalist = Object.entries(dict);
-const vls = Object.values(dict);
-const vlsUniq = [...new Set(vls)];
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const g in vlsUniq) {
+for (const g in valsUniq) {
   const list = [];
   for (const k in totalist) {
-    if (totalist[c][1] === vlsUniq[j]) {
+    if (totalist[c][1] === valsUniq[j]) {
       list.unshift(totalist[c][0]);
     }
   }
-  newDict[vlsUniq[g]] = list;
+  newDict[valsUniq[g]] = list;
 }
 console.log(newDict);
