2e1da704edf23747c07fac01b5b68b281f41640c
wadenya <alexwadenya001@gmail.com>
2023-08-16 01:03:23 -0700

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+
+class Square extends SquareP {
+  charPrint (c) {
+    if (c === undefined){
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let sq = '';
+      for (let p = 0; p < this.width; p++) {
+        sq += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let nmbOccurences = 0;
+  for (let y = 0; y < list.length; y++) {
+    if (searchElement === list[y]) {
+      nmbOccurences++;
+    }
+  }
+  return (nmbOccurences);
+}
