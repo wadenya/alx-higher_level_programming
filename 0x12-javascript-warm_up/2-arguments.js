809782880943f4b5f97dfc922d07b93040a01032
wadenya <alexwadenya001@gmail.com>
2023-08-16 10:15:23 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const cnt = process.argv.length;
-console.log(cnt === 2 ? "No argument" : cnt == 3 ? "Argument found" : "Arguments found");
+console.log(cnt === 2 ? 'No argument' : cnt == 3 ? 'Argument found' : 'Arguments found');
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,12 @@
 const size = Math.floor(Number(process.argv[2]));
 
 if (isNaN(size)) {
-	console.log("Missing size");
+  console.log("Missing size");
 }
 else {
-	for (let y = 0; y < size; y++) {
-		let row = "";
-		for (let p = 0; p < size; p++) row += 'X';
-		console.log(row);
-	}
+  for (let y = 0; y < size; y++) {
+    let row = "";
+    for (let p = 0; p < size; p++) row += 'X';
+    console.log(row);
+  }
 }
