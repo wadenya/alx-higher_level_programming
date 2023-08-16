8b48b23be0075110606bc96e4b789a63fc037d58
wadenya <alexwadenya001@gmail.com>
2023-08-16 01:32:12 -0700

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-
-const Rectangle = require('./5-square');
+const SquareP = require('./5-square');
 
 class Square extends SquareP {
   charPrint (c) {
-    if (c === undefined){
+    if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      let sq = '';
-      for (let p = 0; p < this.width; p++) {
-        sq += c;
+      let q = '';
+      for (let d = 0; d < this.width; d++) {
+        q += c;
       }
-      console.log(sq);
+      console.log(q);
     }
   }
 }
