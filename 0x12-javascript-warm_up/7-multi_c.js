10903c0d0ab1c609d9dccb219f6f2ca67c3f7d3c
wadenya <alexwadenya001@gmail.com>
2023-08-16 11:34:08 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,8 @@
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
-} else {
+}
+else {
   for (let y = 0; y < x; y++) {
     console.log('C is fun');
   }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 const size = Math.floor(Number(process.argv[2]));
-
 if (isNaN(size)) {
   console.log('Missing size');
-}
-else {
-  for (let y = 0; y < size; y++) {
+} else {
+  for (let i = 0; i < size; i++) {
     let row = '';
-    for (let p = 0; p < size; p++) row += 'X';
+    for (let k = 0; k < size; k++) row += 'X';
     console.log(row);
   }
 }
