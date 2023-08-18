930e9e0d5b2a08e141ca966d1eea639daca5ba19
wadenya <alexwadenya001@gmail.com>
2023-08-17 23:13:05 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-""" 
-lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
+"""  script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
-            passwd=argv[2], db=argv[3], port=3306)
+                         passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
             WHERE name LIKE BINARY 'N%'
@@ -16,5 +13,3 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
