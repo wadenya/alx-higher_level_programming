f9f1a83dbab1cadb40774d5a8551e2cfff5482fe
wadenya <alexwadenya001@gmail.com>
2023-08-17 23:29:09 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,10 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
-            passwd=argv[2], db=argv[3], port=3306)
+                         passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'
             ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,11 +9,9 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
-            passwd=argv[2], db=argv[3], port=3306)
+                         passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    cur.execute("SELECT * FROM states name LIKE %s", (match, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
