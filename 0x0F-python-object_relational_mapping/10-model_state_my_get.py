884a98dee1cd80f3f1d89e5aa6897d2798c6e83f
wadenya <alexwadenya001@gmail.com>
2023-08-18 04:10:02 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,11 +10,10 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(argv[1], argv[2], argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     instance = session.query(State).filter(State.name == (argv[4],))
     try:
         print(instance[0].id)
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,10 +10,9 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(argv[1], argv[2], argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     for instance in session.query(State).filter(State.name.like('%a%')):
         print('{0}: {1}'.format(instance.id, instance.name))
