fd6e2e0c459e8405eb1de904d0ae5ec5ddc84479
wadenya <alexwadenya001@gmail.com>
2023-08-18 02:48:40 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM
+    cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s", (sys.argv[4],))
+                WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
     temp = list(row[0] for row in rows)
     print(*temp, step=", ")
