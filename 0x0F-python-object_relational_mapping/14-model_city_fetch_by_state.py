aa55c9e862ca73c6d6512fb90833035580ff76eb
wadenya <alexwadenya001@gmail.com>
2023-08-18 06:40:24 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,9 +4,9 @@ script 14-model_city_fetch_by_state.py that prints all City
 objects from the database hbtn_0e_14_usa:
 """
 from sys import argv
-from model_state import Base, State
+from model_state import State, Base
 from model_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
