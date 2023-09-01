832715400cc65af120b54f6c91fb6243a7ee9689
wadenya <alexwadenya001@gmail.com>
2023-09-01 06:43:56 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,5 +4,7 @@ import urllib.request
 import sys
 
 if len(sys.argv) > 1:
-    with urllib.request.urlopen(urllib.request.Request(sys.argv[1])) as response:
+
+    with urllib.request.urlopen(urllib.request.Request(sys.argv[1]))
+    as response:
         print(dict(response.headers).get('X-Request-Id'))
